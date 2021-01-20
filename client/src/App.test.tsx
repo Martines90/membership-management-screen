e80369974c78f35d4bad127b78d807cd25d96b34
Márton Horváth <hmartonsandor@gmail.com>
2021-01-20 23:49:36 +0100

    update test, find select box by Id

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { queryByAttribute, render } from '@testing-library/react';
 import App from './App';
 
+const getById = queryByAttribute.bind(null, 'id');
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/React Tutorials/i);
+  const dom = render(<App />);
+  const linkElement = getById(dom.container, 'age-native-simple');
   expect(linkElement).toBeInTheDocument();
 });
